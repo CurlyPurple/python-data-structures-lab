342b292623ae230831528c939876affd88f7e83b
AlexG <Alexanderg.savitr@gmail.com>
2024-10-03 11:30:59 -0500

    complete lab except for 5

--- exercises.py
+++ exercises.py
@@ -19,20 +19,26 @@ def manage_students():
 # Use a for loop to iterate over the strings in foods and append each string to meal.
 
 def combine_foods():
-    
+    foods = ('Spaghetti', 'Tacos', 'Burgers')
+    meal = []
+    for food in foods:
+        meal.append(food)
+    return meal
 
 # Call the function and print the result
-print('Exercise 2:', combine_foods())
+# print('Exercise 2:', combine_foods())
 
 # Exercise 3: Slicing Tuples
 #
 # Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
 
 def slice_foods():
-    # your code here
+    foods = ('Spaghetti', 'Tacos', 'Burgers')
+    last_two_foods = foods[1:]
+    return last_two_foods
 
 # Call the function and print the result
-print('Exercise 3:', slice_foods())
+# print('Exercise 3:', slice_foods())
 
 # Exercise 4: Dictionaries and String Formatting
 #
@@ -40,10 +46,16 @@ print('Exercise 3:', slice_foods())
 # Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
 
 def hometown_info():
-    # your code here
+    home_town = {
+        'city': 'Chicago',
+        'state': 'Illinois',
+        'population': '2.665 million'
+    }
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    return home_town_message
 
 # Call the function and print the result
-print('Exercise 4:', hometown_info())
+# print('Exercise 4:', hometown_info())
 
 # Exercise 5: Iterating Over Dictionary Items
 #
@@ -51,7 +63,15 @@ print('Exercise 4:', hometown_info())
 # Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
 
 def list_home_town_items():
-    # your code here
+    home_town = {
+        'city': 'Chicago',
+        'state': 'Illinois',
+        'population': '2.665 million'
+    }
+    home_town_items = []
+    
+    return home_town_items
+
 
 # Call the function and print the result
 print('Exercise 5:', list_home_town_items())
